8568aa46efb58f84a8244454b709c36b52ee5d9a
Lera <lera_kostenko16@mail.ru>
2024-01-20 20:06:14 +0600

    firs commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// let incr = 10,
+//     decr = 10;
+
+//     // префикс
+// // ++incr;
+// // --decr;
+//     // постфикс
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+// console.log(123%10);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+// &&
+// ||
+
+const isChecked = false,
+    isClosed = false;
+
+console.log(isChecked || !isClosed);
+
+
+console.log(((3 > 2) > 1));
+
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`hello, ${user}`);
+
+// alert('hello');
+
+// const result = confirm('Are you herre?');
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18 лет?", "18");
+// console.log(typeof(answer));
+
+
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как вас зовут?', '');
+// answers[1] = prompt('Какая у вас фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+
+
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// // console.log(obj.age);
+// // console.log(obg.isMarried);
+// console.log(obj['name']);
+
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+// console.log(arr[1]);
